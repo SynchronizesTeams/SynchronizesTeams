1d9c4295b9dd07654ac2d866e319e22d9325c815
SynchronizesTeams <146453629+SynchronizesTeams@users.noreply.github.com>
2023-09-29 21:13:11 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="shortcut icon" href="img/icon.png" type="image/x-icon">
+</head>
+  <body>
+    <header>
+      <div class="container">
+        <div id="branding">
+          <h1><span class="highlight">SynchronizeTeams</span> Web Design</h1>
+        </div>
+        <nav>
+          <ul>
+            <li class="current"><a href="index.html">Home</a></li>
+            <li><a href="about.html">About</a></li>
+            <li><a href="index.php">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+
+    <section id="showcase">
+      <div class="container">
+        <h1>The Teams</h1>
+        <p class="title">Our Final Project</p>
+      </div>
+    </section>
+
+    <section id="newsletter">
+      <div class="container">
+        <h1>Follow Us</h1>
+      </div>
+    </section>
+
+    <section id="boxes">
+      <div class="container">
+        <div class="box">
+          <img src="img/instagram-logo.png">
+          <h3><a href="https://www.instagram.com/synchronize_teams/" target="_blank">Instagram</a></h3>
+        </div>
+        <div class="box">
+          <img src="img/github.png">
+          <h3><a href="https://github.com/SynchronizeTeams" target="_blank">Github</a></h3>
+        </div>
+        <div class="box">
+          <img src="img/twitter.png">
+          <h3><a href="https://twitter.com/Synchroniz89440" target="_blank">Twitter</a></h3>
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <p>SynchronizeTeams©</p>
+    </footer>
+  </body>
+</html>
